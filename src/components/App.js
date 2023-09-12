9bace687b8b47c354fed37dbc7498b8299e678d4
elpatron <aidanseantucker@gmail.com>
2023-09-12 15:00:29 -0400

    need to revisit, can't figure it out now

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+// App.js
+import React, { Component } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -7,15 +8,56 @@ import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
-function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: TASKS,
+      selectedCategory: "All",
+    };
+  }
+
+  // Function to handle category selection
+  handleCategorySelect = (category) => {
+    this.setState({ selectedCategory: category });
+  };
+
+  // Function to add a new task
+  handleTaskAdd = (newTask) => {
+    this.setState((prevState) => ({
+      tasks: [...prevState.tasks, newTask],
+    }));
+  };
+
+  // Function to delete a task
+  handleTaskDelete = (taskId) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.filter((task) => task.id !== taskId),
+    }));
+  };
+
+  render() {
+    const { tasks, selectedCategory } = this.state;
+    return (
+      <div className="App">
+        <h2>My tasks</h2>
+        <CategoryFilter
+          categories={CATEGORIES}
+          selectedCategory={selectedCategory}
+          onSelectCategory={this.handleCategorySelect}
+        />
+        <NewTaskForm
+          categories={CATEGORIES}
+          onTaskFormSubmit={this.handleTaskAdd}
+        />
+        <TaskList
+          tasks={tasks}
+          selectedCategory={selectedCategory}
+          onDeleteTask={this.handleTaskDelete}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,65 @@
-import React from "react";
+// NewTaskForm.js
+import React, { Component } from "react";
 
-function NewTaskForm() {
-  return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
-  );
+class NewTaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      category: "All",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { text, category } = this.state;
+    if (text) {
+      const newTask = {
+        id: Date.now(),
+        text,
+        category,
+      };
+      this.props.onTaskFormSubmit(newTask);
+      this.setState({
+        text: "",
+        category: "All",
+      });
+    }
+  };
+
+  render() {
+    const { categories } = this.props;
+    const { text, category } = this.state;
+    return (
+      <form className="new-task-form" onSubmit={this.handleSubmit}>
+        <label>
+          Details
+          <input
+            type="text"
+            name="text"
+            value={text}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Category
+          <select name="category" value={category} onChange={this.handleChange}>
+            {categories.map((cat) => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </label>
+        <input type="submit" value="Add task" />
+      </form>
+    );
+  }
 }
 
 export default NewTaskForm;
+
